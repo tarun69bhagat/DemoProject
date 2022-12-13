14e28e2152180220e4a1f01217529fd19ee8aa00
tarun.bhagat <tarun.bhagat@windowmaker.com>
2022-12-13 11:45:45 +0530

    Initial Commit

---
+++ Demo Translation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo Translation
+//
+//  Created by Tarun Bhagat on 13/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
