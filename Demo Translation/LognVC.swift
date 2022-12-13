ef5d42108b2ed2802c2b5d62b718d6e8fde7d89d
tarun.bhagat <tarun.bhagat@windowmaker.com>
2022-12-13 14:03:20 +0530

    Commit Changes

---
+++ Demo Translation/LognVC.swift
@@ -0,0 +1,16 @@
+//
+//  LognVC.swift
+//  Demo Translation
+//
+//  Created by Tarun Bhagat on 13/12/22.
+//
+
+import Foundation
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
